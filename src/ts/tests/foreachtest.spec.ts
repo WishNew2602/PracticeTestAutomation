621eab0e3bc4d8d3958e19f66d36881bd77374ce
WishNew2602 <Vishnu14321432@gmail>
2026-04-23 09:26:33 +0530

    new commit to master

--- src/ts/tests/foreachtest.spec.ts
+++ src/ts/tests/foreachtest.spec.ts
@@ -1,6 +1,18 @@
 import {test, expect} from '@playwright/test';
 
 
+test('Handle the Iframe', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/tests/htmlfiles/frames.html');
+
+  const topSectionFrame = page
+    .frameLocator('frame[name="main"]')
+    .frameLocator('frame[name="topsection"]');
+
+  await expect(topSectionFrame.locator('h3')).toHaveText('Nested Frame: Top Section');
+
+  const headerVal = await topSectionFrame.locator('h3').textContent();
+  console.log('Top section header:', headerVal);
+});
 
     // test('Test with values', async ({ page }) => {
     //         await page.goto('https://practicetestautomation.com/practice-test-login/');
