b33acf214bb5e9b1d1f2b8baf69cfc7794a14bbe
WishNew2602 <Vishnu14321432@gmail>
2026-04-23 09:09:47 +0530

    adding Search product Test

--- src/ts/tests/amzontest.spec.ts
+++ src/ts/tests/amzontest.spec.ts
@@ -2,6 +2,22 @@ import {test, expect} from '@playwright/test';
 
 test.skip('Search a product', async({page}) => {
 
+    test.beforeEach('before each', async({page}) =>{
+    console.log('before each');
+    page.on('dialog', async dialog => {
+        console.log(dialog.message());
+        console.log(dialog.type());
+        if(dialog.type() === 'prompt'){
+            await dialog.accept('Vishnu');
+        }
+        if(dialog.type() === 'alert'){
+            await dialog.accept();
+        }
+        if(dialog.type() === 'confirm'){
+            await dialog.dismiss();
+        }
+    }); //event defination for dialog   
+});
     const searchTerm = 'Iphone';
     console.log(searchTerm);
     await page.goto('https://amazon.com/');
@@ -13,5 +29,13 @@ test.skip('Search a product', async({page}) => {
     // await page.locator('.nav-search-field > input[id="twotabsearchtextbox"]').clear()
     // await page.locator('.nav-search-field > input[id="twotabsearchtextbox"]').press('Enter');
     // await page.getByText('Sell').click();
+    test('different util methods', async({page}) => {
+        await page.goto('https://www.tutorialspoint.com/selenium/practice/register.php');
+        // await page.getByPlaceholder('First Name').fill(TestDataUtil.getFirstName());
+        // await page.getByPlaceholder('lastname').fill(TestDataUtil.getLastName());
+        // await page.getByPlaceholder('UserName').fill(TestDataUtil.getUsername());
+        // await page.getByPlaceholder('Password').fill('Password123');
+    });
+
 
 })
